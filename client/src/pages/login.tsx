1de2d12dee50df9e0857ef42ac0584d59148349f
Ameriq8 <wwwwwwssssss91@gmail.com>
2023-10-08 11:48:43 +0300

    All site files moved to client folder

---
+++ client/src/pages/login.tsx
@@ -0,0 +1,10 @@
+import NavbarComponent from '@/components/layouts/navbar'
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div>
+        <NavbarComponent />
+    </div>
+  )
+}
