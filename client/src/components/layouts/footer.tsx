3acd543908e3203f105d8e5224234006d9b3aa04
Ameriq8 <wwwwwwssssss91@gmail.com>
2023-10-09 23:32:16 +0300

    Some changed in landing page

--- client/src/components/layouts/footer.tsx
+++ client/src/components/layouts/footer.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 
-export default function FooterComponent() {
+export default function FooterComponent(): React.JSX.Element {
   return (
     <footer className="my-auto relative">
       <div className="container px-6 py-8 mx-auto">
@@ -38,11 +38,17 @@ export default function FooterComponent() {
           </div>
         </div>
 
-        <hr className="my-6 border-gray-200 md:my-10" />
+        <div className="flex justify-center my-6 md:my-10">
+          <span className="inline-block w-9/12 h-1 bg-blue-500 rounded-full"></span>
+          <span className="inline-block w-2/12 h-1 mx-1 bg-blue-500 rounded-full"></span>
+          <span className="inline-block w-1/12 h-1 bg-blue-500 rounded-full"></span>
+        </div>
 
         <div className="text-center text-sm text-gray-500">
           تم العمل على هذا الموقع بواسطة المبرمج{' '}
-          <a href='https://www.instagram.com/56_bc' className='text-indigo-500 hover:underline'>عامر محمد</a>
+          <a href="https://www.instagram.com/56_bc" className="text-indigo-500 hover:underline">
+            عامر محمد
+          </a>
         </div>
       </div>
     </footer>
